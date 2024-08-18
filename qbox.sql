8d634f2777f38d82249f1f30933e6cea4f57cebc
Preston <35425544+lPrestonl@users.noreply.github.com>
2024-08-18 12:08:51 -0400

    Update qbox.sql

--- qbox.sql
+++ qbox.sql
@@ -137,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `players` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 ALTER TABLE `players`
-ADD IF NOT EXISTS `last_logged_out` timestamp NULL DEFAULT NULL AFTER `last_updated`,
+ADD `last_logged_out` timestamp NULL DEFAULT NULL AFTER `last_updated`,
 MODIFY COLUMN `citizenid` varchar(50) NOT NULL COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `playerskins` (
